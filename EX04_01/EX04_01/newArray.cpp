fb053b5440fd8255ad43f3edf94144b8c6e684c8
lsmith17 <lsmith17@my.whitworth.edu>
2014-06-05 20:39:00 -0700

    Added Code

---
+++ EX04_01/EX04_01/newArray.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	int arraysize;
+	cout << "What is the array size?" << endl;
+	cin >> arraysize;
+
+	int *number = new int[arraysize];
+	int size = 0;
+
+	for (int i = 0; i <= arraysize; i++){
+		int value;
+		cout << "Enter in value";
+		cin >> value;
+
+		if ()
+	}
+}
